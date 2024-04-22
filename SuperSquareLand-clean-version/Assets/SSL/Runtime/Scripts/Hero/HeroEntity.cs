08a0caeabaf220050238cfa64a756dd9c29ca447
TygerTac <ferhat.bulot@edu.devinci.fr>
2024-04-22 12:30:05 +0200

    Addition du truc.

---
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
@@ -0,0 +1,90 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class HeroEntity : MonoBehaviour
+{
+    [Header("Physics")]
+    [SerializeField] private Rigidbody2D _rigidbody;
+
+    [Header("Horizontal Movements")]
+    [SerializeField] private HeroHorizontalMovementSettings _movementsSettings;
+    private float _horizontalSpeed = 0f;
+    private float _moveDirX = 0f;
+
+    [Header("Orientation")]
+    [SerializeField] private Transform _orientVisualRoot;
+    private float _orientX = 1f;
+
+    [Header("Debug")]
+    [SerializeField] private bool _guiDebug = false;
+
+    public void SetMoveDirX(float dirX)
+    {
+        _moveDirX = dirX;
+    }
+
+    private void FixedUpdate()
+    {
+        _ApplyHorizontalSpeed();
+        _ChangeOrientFromHorizontalMovement();
+        _ApplyHorizontalSpeed();
+    }
+
+    private void _ChangeOrientFromHorizontalMovement()
+    {
+        if(_moveDirX == 0f) 
+        return;
+        _orientX = Mathf.Sign(_moveDirX);
+    }
+
+    private void _ApplyHorizontalSpeed()
+    {
+        Vector2 velocity = _rigidbody.velocity;
+        velocity.x = _horizontalSpeed * _moveDirX;
+        _rigidbody.velocity = velocity;
+    }
+
+    private void _UpdateHorizonSpeed()
+    {
+        if(_moveDirX != 0f)
+        {
+            _Accelerate();
+        } else {
+            _horizontalSpeed = 0f;
+        }
+    }
+
+
+    private void _Accelerate()
+    {
+        _horizontalSpeed += _movementsSettings.acceleration * Time.fixedDeltaTime;
+        if (_horizontalSpeed > _movementsSettings.speedMax) 
+        {
+            _horizontalSpeed = _movementsSettings.speedMax;
+        }
+    }
+    
+    private void Update()
+    {
+        _UpdateOrientVisual();
+    }
+
+    private void _UpdateOrientVisual()
+    {
+        Vector3 newScale = _orientVisualRoot.localScale;
+        newScale.x = _orientX;
+        _orientVisualRoot.localScale = newScale;
+    }
+
+    private void OnGUI()
+    {
+        if (!_guiDebug) return;
+
+        GUILayout.BeginVertical(GUI.skin.box);
+        GUILayout.Label(gameObject.name);
+        GUILayout.Label($"MoveDirX = {_moveDirX}");
+        GUILayout.Label($"OrientX = {_orientX}");
+        GUILayout.Label($"Horizontal Speed = {_horizontalSpeed}");
+        GUILayout.EndVertical();
+    }
+}
---
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroHorizontalMovementSettings.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class HeroHorizontalMovementSettings
+{
+    public float acceleration = 20f;
+    public float speedMax = 5f;
+}
