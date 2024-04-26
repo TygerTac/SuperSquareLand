809a5824526d1aef923041f0d9bcf775da42b6f0
TygerTac <ferhat.bulot@edu.devinci.fr>
2024-04-26 11:10:59 +0200

    commit

--- SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -1,6 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Net.Http.Headers;
+using UnityEngine;
 
 public class CameraProfile : MonoBehaviour
 {
+    private Camera _camera;
+    public float CameraSize => _camera.orthographicSize;
+    public Vector3 Position => _camera.transform.position;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        if(_camera != null )
+        {
+            _camera.enabled = false;
+        }
+    }
 }
+
 
